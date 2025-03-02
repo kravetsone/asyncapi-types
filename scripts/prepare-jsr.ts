a2ae0f83d741f5d669056a0bc2e6ff93f4d0b658
kravetsone <57632712+kravetsone@users.noreply.github.com>
2025-03-03 02:59:45 +0300

    chore: jsr.json

--- scripts/prepare-jsr.ts
+++ scripts/prepare-jsr.ts
@@ -2,19 +2,19 @@ import { execSync } from "node:child_process";
 import fs from "node:fs";
 
 const version = execSync("npm pkg get version")
-	.toString()
-	.replace(/"|\n/gi, "");
+    .toString()
+    .replace(/"|\n/gi, "");
 
-const jsrConfig = JSON.parse(String(fs.readFileSync("deno.json")));
+const jsrConfig = JSON.parse(String(fs.readFileSync("jsr.json")));
 
 jsrConfig.version = version;
 
-fs.writeFileSync("deno.json", JSON.stringify(jsrConfig, null, 4));
+fs.writeFileSync("jsr.json", JSON.stringify(jsrConfig, null, 4));
 
 try {
-	execSync("bun x @teidesu/slow-types-compiler@latest fix --entry deno.json");
+    execSync("bun x @teidesu/slow-types-compiler@latest fix --entry jsr.json");
 } catch (error) {
-	console.error(error);
+    console.error(error);
 }
 
 console.log("Prepared to release on JSR!");
